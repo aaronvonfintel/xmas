b1caedecedde879b02e7b4fa4dc09ddf3c16a33c
Marion Hanke <marion.hanke@designaffairs.com>
2015-12-09 12:05:42 +0100

    update animation preparation

--- js/myFullpage.js
+++ js/myFullpage.js
@@ -8,25 +8,34 @@
         'scrollOverflow': true,
         'scrollbar': true,
         'afterLoad': function (anchorLink, index) {
+
             if (index == 2) {
-                $('#iphone3, #iphone2, #iphone4').addClass('active');
+                //$('#iphone3, #iphone2, #iphone4').addClass('active');
+
+                $('#text02').addClass('active');
+                alert("index 2");
             }
+
         },
 
         'onLeave': function (index, nextIndex, direction) {
             if (index == 3 && direction == 'down') {
                 $('.section').eq(index - 1).removeClass('moveDown').addClass('moveUp');
+                //alert("down 3");
             }
             else if (index == 3 && direction == 'up') {
                 $('.section').eq(index - 1).removeClass('moveUp').addClass('moveDown');
+                //alert("up 3");
             }
             else if (index == 4 && direction == 'down') {
                 $('.section').eq(index - 1).removeClass('moveDown').addClass('moveUp');
+                //alert("down 4");
 
             }
-            else if (index == 4 && direction == 'up') {
-                $('.section').eq(index - 1).removeClass('moveUp').addClass('moveDown');
-            }
+            //else if (index == 4 && direction == 'up') {
+            //    $('.section').eq(index - 1).removeClass('moveUp').addClass('moveDown');
+
+            //}
 
             $('#staticImg').toggleClass('active', (index == 2 && direction == 'down') || (index == 4 && direction == 'up'));
             $('#staticImg').toggleClass('moveDown', nextIndex == 4);
