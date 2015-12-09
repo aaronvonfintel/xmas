6da9b281e2bfa52b4991b0131a79f2f2eada10fe
Aaron von Fintel <vonFintel@Administrators-MacBook-Pro-3.local>
2015-12-09 10:33:00 +0100

    First commit of all files

---
+++ js/myWaypoint.js
@@ -0,0 +1,330 @@
+﻿$(function () {
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_01"),
+        handler: function () {
+            document.getElementById("gifStill_01").src = "assets/image/gifs/01.gif";
+            //alert("Gif 1");
+        },
+        offset: '50%', 
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_02"),
+        handler: function () {
+            document.getElementById("gifStill_02").src = "assets/image/gifs/02.gif";
+        },
+        offset: '50%',
+        context: document.getElementById('basic-waypoint')})
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_04"),
+        handler: function () {
+            document.getElementById("gifStill_04").src = "assets/image/gifs/04.gif";
+        },
+        offset: '50%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_05"),
+        handler: function () {
+            document.getElementById("gifStill_05").src = "assets/image/gifs/05.gif";
+        },
+        offset: '50%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_06"),
+        handler: function () {
+            document.getElementById("gifStill_06").src = "assets/image/gifs/06.gif";
+        },
+        offset: '50%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_07"),
+        handler: function () {
+            document.getElementById("gifStill_07").src = "assets/image/gifs/07.gif";
+        },
+        offset: '50%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_08"),
+        handler: function () {
+            document.getElementById("gifStill_08").src = "assets/image/gifs/08.gif";
+        },
+        offset: '50%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_09"),
+        handler: function () {
+            document.getElementById("gifStill_09").src = "assets/image/gifs/09.gif";
+        },
+        offset: '50%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_10"),
+        handler: function () {
+            document.getElementById("gifStill_10").src = "assets/image/gifs/10.gif";
+        },
+        offset: '50%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_11"),
+        handler: function () {
+            document.getElementById("gifStill_11").src = "assets/image/gifs/11.gif";
+        },
+        offset: '50%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_12"),
+        handler: function () {
+            document.getElementById("gifStill_12").src = "assets/image/gifs/12.gif";
+        },
+        offset: '50%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_13"),
+        handler: function () {
+            document.getElementById("gifStill_13").src = "assets/image/gifs/13.gif";
+        },
+        offset: '50%',
+        context: document.getElementById('basic-waypoint')
+    })
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Stop
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_01"),
+        handler: function () {
+            document.getElementById("gifStill_01").src = "assets/image/jpg/01.jpg";
+        },
+        offset: '80%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_02"),
+        handler: function () {
+            document.getElementById("gifStill_02").src = "assets/image/jpg/02.jpg";
+        },
+        offset: '80%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_04"),
+        handler: function () {
+            document.getElementById("gifStill_04").src = "assets/image/jpg/04.jpg";
+        },
+        offset: '80%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_05"),
+        handler: function () {
+            document.getElementById("gifStill_05").src = "assets/image/jpg/05.jpg";
+        },
+        offset: '80%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_06"),
+        handler: function () {
+            document.getElementById("gifStill_06").src = "assets/image/jpg/06.jpg";
+        },
+        offset: '80%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_07"),
+        handler: function () {
+            document.getElementById("gifStill_07").src = "assets/image/jpg/07.jpg";
+        },
+        offset: '80%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_08"),
+        handler: function () {
+            document.getElementById("gifStill_08").src = "assets/image/jpg/08.jpg";
+        },
+        offset: '80%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_09"),
+        handler: function () {
+            document.getElementById("gifStill_09").src = "assets/image/jpg/09.jpg";
+        },
+        offset: '80%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_10"),
+        handler: function () {
+            document.getElementById("gifStill_10").src = "assets/image/jpg/10.jpg";
+        },
+        offset: '80%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_11"),
+        handler: function () {
+            document.getElementById("gifStill_11").src = "assets/image/jpg/11.jpg";
+        },
+        offset: '80%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_12"),
+        handler: function () {
+            document.getElementById("gifStill_12").src = "assets/image/jpg/12.jpg";
+        },
+        offset: '80%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_13"),
+        handler: function () {
+            document.getElementById("gifStill_13").src = "assets/image/jpg/13.jpg";
+        },
+        offset: '80%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Stop
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_01"),
+        handler: function () {
+            document.getElementById("gifStill_01").src = "assets/image/jpg/01.jpg";
+        },
+        offset: '20%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_02"),
+        handler: function () {
+            document.getElementById("gifStill_02").src = "assets/image/jpg/02.jpg";
+        },
+        offset: '20%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_04"),
+        handler: function () {
+            document.getElementById("gifStill_04").src = "assets/image/jpg/04.jpg";
+        },
+        offset: '20%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_05"),
+        handler: function () {
+            document.getElementById("gifStill_05").src = "assets/image/jpg/05.jpg";
+        },
+        offset: '20%',
+        context:
+        document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_06"),
+        handler: function () {
+            document.getElementById("gifStill_06").src = "assets/image/jpg/06.jpg";
+        },
+        offset: '20%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_07"),
+        handler: function () {
+            document.getElementById("gifStill_07").src = "assets/image/jpg/07.jpg";
+        },
+        offset: '20%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_08"),
+        handler: function () {
+            document.getElementById("gifStill_08").src = "assets/image/jpg/08.jpg";
+        },
+        offset: '20%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_09"),
+        handler: function () {
+            document.getElementById("gifStill_09").src = "assets/image/jpg/09.jpg";
+        },
+        offset: '20%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_10"),
+        handler: function () {
+            document.getElementById("gifStill_10").src = "assets/image/jpg/10.jpg";
+        },
+        offset: '20%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_11"),
+        handler: function () {
+            document.getElementById("gifStill_11").src = "assets/image/jpg/11.jpg";
+        },
+        offset: '20%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_12"),
+        handler: function () {
+            document.getElementById("gifStill_12").src = "assets/image/jpg/12.jpg";
+        },
+        offset: '20%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+    var waypoint = new Waypoint({
+        element: document.getElementById("gifStill_13"),
+        handler: function () {
+            document.getElementById("gifStill_13").src = "assets/image/jpg/13.jpg";
+        },
+        offset: '20%',
+        context: document.getElementById('basic-waypoint')
+    })
+
+});
